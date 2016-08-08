319d234bc253da22269045b8c3281c859b04333a
Todd Kersey <todd.kersey@iga.in.gov>
2016-08-08 14:38:58 -0400

    Update json url to be relative

--- nalit/www/js/services.js
+++ nalit/www/js/services.js
@@ -6,13 +6,13 @@ app.factory('Data', function($http) {
 
   return {
     all: function(){
-      return $http.get('http://localhost:8100/data.json').then(function(response) {
+      return $http.get('/data.json').then(function(response) {
         data = response.data;
         return data;
       });
     },
     get: function(itemId) {
-      return $http.get('http://localhost:8100/data.json').then(function(response) {
+      return $http.get('/data.json').then(function(response) {
         data = response.data.data;
         for (var i = 0; i < data.length; i++) {
           if (data[i].id === parseInt(itemId)) {
