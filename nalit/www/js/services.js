a3ba373f34d09115e6c81b151d3f3a5a0246f5cf
Todd Kersey <todd.kersey@iga.in.gov>
2016-08-16 11:42:48 -0400

    Add domain for content in order to get json to work locally

--- nalit/www/js/services.js
+++ nalit/www/js/services.js
@@ -3,16 +3,22 @@ var app = angular.module('nalit.services', []);
 app.factory('Data', function($http) {
 
   var data = [];
+  var get_url = "";
 
+  if (window.location.hostname != "localhost") {
+    get_url = 'http://nalit.iga.local/assets/data.json';
+  } else {
+    get_url = 'assets/data.json';
+  }
   return {
     all: function(){
-      return $http.get('assets/data.json').then(function(response) {
+      return $http.get(get_url).then(function(response) {
         data = response.data;
         return data;
       });
     },
     get: function(itemId) {
-      return $http.get('assets/data.json').then(function(response) {
+      return $http.get(get_url).then(function(response) {
         data = response.data.data;
         for (var i = 0; i < data.length; i++) {
           if (data[i].id === parseInt(itemId)) {
